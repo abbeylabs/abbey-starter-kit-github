8daf44fb48714976561cf7c52c7d0a139381afce
Jeff Chao <jeffchao@me.com>
2023-07-10 21:51:11 -0700

    Use managed state backend in example.

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-#  backend "http" {
-#    address        = "https://api.abbey.io/terraform-http-backend"
-#    lock_address   = "https://api.abbey.io/terraform-http-backend/lock"
-#    unlock_address = "https://api.abbey.io/terraform-http-backend/unlock"
-#    lock_method    = "POST"
-#    unlock_method  = "POST"
-#  }
+  backend "http" {
+    address        = "https://api.abbey.io/terraform-http-backend"
+    lock_address   = "https://api.abbey.io/terraform-http-backend/lock"
+    unlock_address = "https://api.abbey.io/terraform-http-backend/unlock"
+    lock_method    = "POST"
+    unlock_method  = "POST"
+  }
 
   required_providers {
     abbey = {
